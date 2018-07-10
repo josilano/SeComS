140c710b9d87f9848a6f8fd2dd0ff09c6a501ebb
Lano <artud_p@hotmail.com>
2018-07-10 20:30:43 -0300

    criando css do materialize e js

---
+++ public/js/init.js
@@ -0,0 +1,62 @@
+/**
+ * Created by LaNo on 03/05/2017.
+ */
+$(document).ready(function(){
+    $('#observacoes').val('');
+    $('#observacoes').trigger('autoresize');
+    Materialize.updateTextFields();
+    $('.modal').modal();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        format: 'dd/mm/yyyy',
+        closeOnSelect: false, // Close upon selecting a date,
+        container: undefined, // ex. 'body' will append picker to body
+        monthsFull: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        weekdaysFull: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+        today: 'Hoje',
+        clear: 'Limpar',
+        close: 'Pronto',
+        labelMonthNext: 'Próximo mês',
+        labelMonthPrev: 'Mês anterior',
+        labelMonthSelect: 'Selecione um mês',
+        labelYearSelect: 'Selecione um ano'
+    });
+});
+
+
+function maskIt(w,e,m,r,a){
+// Cancela se o evento for Backspace
+    if (!e) var e = window.event
+    if (e.keyCode) code = e.keyCode;
+    else if (e.which) code = e.which;
+// Variáveis da função
+        var txt  = (!r) ? w.value.replace(/[^\d]+/gi,'') : w.value.replace(/[^\d]+/gi,'').reverse();
+        var mask = (!r) ? m : m.reverse();
+        var pre  = (a ) ? a.pre : "";
+        var pos  = (a ) ? a.pos : "";
+        var ret  = "";
+    if(code == 9 || code == 8 || txt.length == mask.replace(/[^#]+/g,'').length) return false;
+// Loop na máscara para aplicar os caracteres
+    for(var x=0,y=0, z=mask.length;x<z && y<txt.length;){
+        if(mask.charAt(x)!='#'){
+            ret += mask.charAt(x); x++; }
+        else {
+            ret += txt.charAt(y); y++; x++; } }
+// Retorno da função
+    ret = (!r) ? ret : ret.reverse()
+    w.value = pre+ret+pos; }
+// Novo método para o objeto 'String'
+    String.prototype.reverse = function(){
+    return this.split('').reverse().join('');
+};
+
+function number_format( number, decimals, dec_point, thousands_sep ) {
+    var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
+    var d = dec_point == undefined ? "," : dec_point;
+    var t = thousands_sep == undefined ? "." : thousands_sep, s = n < 0 ? "-" : "";
+    var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+}
