6ef086abdb679505ed4ac8bab7c12442c38a3d24
Lano <artud_p@hotmail.com>
2018-07-10 21:15:43 -0300

    criacao das partes do template base

---
+++ view/partials/head.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title><?php echo $title; ?></title>
+
+    <meta name="description" content="Teste prático de um e-commerce simples">
+    <meta name="author" content="Lano">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="<?php echo (isset($dircss) ? $dircss : null); ?>public/css/materialize.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo (isset($dircss) ? $dircss : null); ?>public/css/stylecid.css" type="text/css">
+    <link href="<?php echo (isset($dircss) ? $dircss : null); ?>public/css/style.css" rel="stylesheet">
+</head>
---
+++ view/partials/jscripts.php
@@ -0,0 +1,6 @@
+<!-- JQuery -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<!-- JavaScript -->
+<script type="text/javascript" src="<?php echo (isset($dirjs) ? $dirjs : null); ?>public/js/materialize.min.js"></script>
+<script type="text/javascript" src="<?php echo (isset($dirjs) ? $dirjs : null); ?>public/js/init.js"></script>
+<script src="<?php echo (isset($dirjs) ? $dirjs : null); ?>public/js/scripts.js"></script>
